bf2c739a538c9d7b02762b05785d0b045f753e28
James Walsh <jpw1012@gmail.com>
2020-07-14 08:46:44 -0400

    Init - code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dexcomapi", # Replace with your own username
-    version="0.0.1",
+    version="0.1.0",
     author="Jim Walsh",
     author_email="jim@walshfamily.email",
     description="Uses Dexcom APIs to download blood glucose data",
