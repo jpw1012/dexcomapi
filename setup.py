ec8d7961c3ce0c4dbe35d24b530cfd9c0c2ff4f4
James Walsh <jpw1012@gmail.com>
2020-07-15 09:30:49 -0400

    Correcting date init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dexcomapi",
-    version="0.2.3",
+    version="0.2.5",
     author="Jim Walsh",
     author_email="jim@walshfamily.email",
     description="Uses Dexcom APIs to download blood glucose data",
